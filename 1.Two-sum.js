b02b9fea564db90a4ea3668a2553ea2660d766cd
HaHoang <bhoangha.hp@gmail.com>
2023-09-28 22:50:39 +0700

    feat: problem solving - two sum

---
+++ 1.Two-sum.js
@@ -0,0 +1,19 @@
+var twoSum = function (nums, target) {
+    const comp = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (comp[nums[i]] >= 0) {
+            return [comp[nums[i]], i];
+        }
+        comp[target - nums[i]] = i;
+    }
+};
+
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (target - nums[i] === nums[j]) {
+                return [i, j];
+            }
+        }
+    }
+};
