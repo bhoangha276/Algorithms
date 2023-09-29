10f39bcf33baff2c04c8c8615a12df8c371c5eb3
HaHoang <bhoangha.hp@gmail.com>
2023-09-29 22:52:18 +0700

    feat: problem solving - add two numbers

---
+++ 2. Add-two-numbers.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+const iter = (n1, n2, rest = 0) => {
+    if (!n1 && !n2 && !rest) return null;
+    const newVal = (n1?.val || 0) + (n2?.val || 0) + rest;
+    const nextNode = iter(n1?.next, n2?.next, Math.floor(newVal / 10));
+    return new ListNode(newVal % 10, nextNode);
+};
+
+var addTwoNumbers = function (l1, l2) {
+    return iter(l1, l2);
+};
