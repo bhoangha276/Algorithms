c093e6d7b9e3629ca164973a5d9a1cdd967a1d1f
HaHoang <bhoangha.hp@gmail.com>
2023-09-30 23:34:06 +0700

    feat: problem solving - longest substring without repeating characters

---
+++ 3. Longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    var k = 0;
+    var maxLength = 0;
+    for (i = 0; i < s.length; i++) {
+        for (j = k; j < i; j++) {
+            if (s[i] === s[j]) {
+                k = j + 1;
+                break;
+            }
+        }
+        if (i - k + 1 > maxLength) {
+            maxLength = i - k + 1;
+        }
+    }
+    return maxLength;
+};
