79268303fac407a52adaa0a71581a69947f2a8e9
HaHoang <bhoangha.hp@gmail.com>
2023-10-03 22:05:16 +0700

    feat: problem solving - longest palindromic substring

---
+++ 5. Longest-palindromic-substring.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    const n = s.length;
+    if (n === 0) return "";
+    if (n === 1) return s;
+    
+    let minstart = 0,
+        maxlen = 0;
+    
+    let i = 0;
+    while (i < n) {
+        if (n - i < maxlen / 2) break;
+    
+        let l = i,
+            r = i;
+    
+        // Find the center of the palindrome
+        while (r < n - 1 && s[r] === s[r + 1]) r++;
+    
+        // Update the next starting point
+        i = r + 1;
+    
+        // Expand around the center to find the longest palindrome
+        while (l > 0 && r < n - 1 && s[l - 1] === s[r + 1]) {
+            l--;
+            r++;
+        }
+    
+        const newlen = r - l + 1;
+        if (newlen > maxlen) {
+            maxlen = newlen;
+            minstart = l;
+        }
+    }
+    
+    return s.substring(minstart, minstart + maxlen);
+    
+    };
