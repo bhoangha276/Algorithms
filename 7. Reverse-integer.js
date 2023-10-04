d9b8ad9989b359e477a0a1a5e2e3fcb883255f6a
HaHoang <bhoangha.hp@gmail.com>
2023-10-05 00:27:58 +0700

    feat: problem solving - reverse integer

---
+++ 7. Reverse-integer.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    let rev = 0
+    const sign = x < 0 ? -1 : 1
+    x = Math.abs(x)
+    while (x !== 0) {
+        const digit = x % 10
+        rev = rev * 10 + digit
+        x = Math.floor(x / 10)
+    }
+    const result = sign * rev
+    if (result > 2 ** 31 - 1 || result < -(2 ** 31)) return 0
+    return result
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+var reverse = function (x) {
+    const result =
+        parseFloat(x.toString().split('').reverse().join('')) * Math.sign(x)
+
+    if (result > 2 ** 31 - 1 || result < -(2 ** 31)) return 0
+
+    return result
+}
+
+*/
