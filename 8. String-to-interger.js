cfadbfc657602ac3dfaab2d62d3d9ae6bfb007b8
HaHoang <bhoangha.hp@gmail.com>
2023-10-09 00:28:52 +0700

    feat: problem solving - string to integer

---
+++ 8. String-to-interger.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var myAtoi = function (s) {
+    s = s.trim()
+    if (s.length === 0) {
+        return 0
+    }
+
+    let num = 0
+    let i = 0
+    let sign = 1 // 1 for positive, -1 for negative
+
+    if (s[i] === '+') {
+        i++
+    } else if (s[i] === '-') {
+        i++
+        sign = -1
+    }
+
+    while (i < s.length && /^\d$/.test(s[i])) {
+        num = num * 10 + parseInt(s[i])
+        i++
+    }
+
+    num *= sign
+    num = Math.max(Math.min(num, Math.pow(2, 31) - 1), -Math.pow(2, 31)) // Check for integer overflow
+    return num
+}
